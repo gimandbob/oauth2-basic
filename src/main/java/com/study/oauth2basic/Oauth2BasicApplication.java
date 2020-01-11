7640400299daf811a403258a9ceb9fc88763ab28
gimandbob <soyounyhwang@gmail.com>
2020-01-11 14:28:15 +0900

    First commit

---
+++ src/main/java/com/study/oauth2basic/Oauth2BasicApplication.java
@@ -0,0 +1,13 @@
+package com.study.oauth2basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2BasicApplication.class, args);
+    }
+
+}
